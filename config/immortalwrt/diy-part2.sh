8adb264af8ce1759166400a264dc1565a2bbb9ce
Onlyoooooo <674824021@qq.com>
2025-07-02 13:59:03 +0800

    Update diy-part2.sh

--- config/immortalwrt/diy-part2.sh
+++ config/immortalwrt/diy-part2.sh
@@ -24,12 +24,6 @@ sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By Only'/g" package/base-fil
 # ------------------------------- Other started -------------------------------
 #
 
-# 修改luci-app-store版本号
-# grep -q '^[[:space:]]*PKG_VERSION:=' feeds/kenzo/luci-app-store/Makefile && [[ $(grep '^[[:space:]]*PKG_VERSION:=' feeds/kenzo/luci-app-store/Makefile | cut -d= -f2-) == *-* ]] && sed -i -e "s/^\([[:space:]]*PKG_VERSION:=\).*/\1$(grep '^[[:space:]]*PKG_VERSION:=' feeds/kenzo/luci-app-store/Makefile | cut -d= -f2- | cut -d- -f1)/" -e "/^[[:space:]]*#/b; /^[[:space:]]*PKG_RELEASE:=/c\PKG_RELEASE:=$(grep '^[[:space:]]*PKG_VERSION:=' feeds/kenzo/luci-app-store/Makefile | cut -d= -f2- | cut -d- -f2-)" feeds/kenzo/luci-app-store/Makefile
-
-# 修改首页samba4快速设置路径问题
-# sed -i 's|/cgi-bin/luci/admin/services/samba4|/cgi-bin/luci/admin/nas/samba4|g' feeds/kenzo/luci-app-quickstart/htdocs/luci-static/quickstart/index.js
-
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
